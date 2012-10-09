1141979dd184cbe94a2f21f349924c12eb15a667
iwcho <iwcho@iwcho.(none)>
2012-10-09 10:47:47 +0300

    1st java

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+	public static void main(String[] args) {
+		int arr[] = new int[10];
+		int a=51;
+		for (int i=0; i<10; i++){
+			arr[i]=a;
+			a=a+2;
+		System.out.println(arr[i]);
+
+		}
+		
+	}
+
+}
